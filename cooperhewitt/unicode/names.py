3f5e5ef203506d9b526bb3dce5f7770d1b786f80
straup <github@aaronland.net>
2014-11-25 08:39:24 -0500

    include ucd by default

--- cooperhewitt/unicode/names.py
+++ cooperhewitt/unicode/names.py
@@ -3,30 +3,31 @@
 
 import json
 import logging
+import cooperhewitt.unicode.ucd as ucd
 
 class lookup:
 
-    def __init__(self, data):
-        fh = open(data, 'r')
-        self.lookup = json.load(fh)
+    def __init__(self):
+        pass
 
     def name(self, char):        
         id = ord(char)
         hex = "%04X" % id
 
         logging.debug("char: %s id: %s hex: %s" % (char, id, hex))
-        return self.lookup.get(hex, None)
+        return ucd.UCD_MAP.get(hex, None)
 
 if __name__ == '__main__':
 
     import sys
 
-    source = sys.argv[1]
-    input = sys.argv[2]
+    input = sys.argv[1:]
+    input = " ".join(input)
 
-    char = char.decode('utf-8')
+    input = input.decode('utf-8')
 
-    ref = names.lookup(source)
-    name = ref.name(char)
+    ref = lookup()
 
-    print "%s is %s" % (char, name)
+    for char in input:
+        name = ref.name(char)
+        print "%s is %s" % (char, name)
--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ desc = open("README.md").read(),
 setup(
     name='cooperhewitt.unicode',
     namespace_packages=['cooperhewitt', 'cooperhewitt.unicode'],
-    version='0.1',
+    version='0.11',
     description='Tools for working with Unicode',
     author='Smithsonian Cooper-Hewitt National Design Museum',
     url='https://github.com/cooperhewitt/py-cooperhewitt-unicode',
     requires=[],
     packages=packages,
-    scripts=[],
-    download_url='https://github.com/cooperhewitt/py-cooperhewitt-unicode/releases/tag/v0.1',
+    scripts=[
+        'scripts/ucd-name'
+        ],
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-unicode/releases/tag/v0.11',
     license='BSD')
